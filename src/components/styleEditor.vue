ba510416a44801de9b029d9c1959b314c82e3e80
夕水 <854806732@qq.com>
2020-03-03 00:48:09 +0800

    将代码分离，精简和优化了代码

--- src/components/styleEditor.vue
+++ src/components/styleEditor.vue
@@ -4,33 +4,4 @@
 		<div class="styleEditor" ref="container" contenteditable="true" @blur="updateCode($event)" v-html="highlightedCode"></div>
 	</div>
 </template>
-<script>
-	import Prism from 'prismjs'
-	export default {
-		name:'Editor',
-		props:['code'],
-		computed:{
-			highlightedCode:function(){
-				//代码高亮
-				return Prism.highlight(this.code,Prism.languages.css);
-			},
-			// 让代码生效
-			codeInstyleTag:function(){
-				return `<style>${this.code}</style>`
-			}
-		},
-		methods:{
-			goBottom(){
-				this.$refs.container.scrollTop = 10000;
-			},
-			updateCode(e){
-				this.$emit('update:code',e.target.textContent);
-			}
-		}
-	}
-</script>
-<style scoped>
-	.code{
-		display:none;
-	}
-</style>
+<script src="../js/styleEditor.js"></script>
